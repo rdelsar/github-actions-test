d26c9b362aa8c4972d8b5cf0a5d3b79878ef4279
rdelsar <rodney.rosa0627@gmail.com>
2025-03-08 12:52:20 -0700

    Update hello.rs

--- hello.rs
+++ hello.rs
@@ -1,7 +1,7 @@
 fn main() {
     println!("hello world, today is march first!");
-    let x = 8;
-    let y = 6;
+    let mut x = 8;
+    let mut y = 6;
     println!("{}", x + y);
     x = 7;
     y = 2;
